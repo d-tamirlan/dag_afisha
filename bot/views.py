0a056f9967645420d223ec95d7b4e4e2330dfc98
Citizen <konoor@yandex.ru>
2019-10-04 17:56:42 +0300

    proxy deleted

--- bot/views.py
+++ bot/views.py
@@ -326,12 +326,3 @@ dag_afisha_bot.message_handler(regexp='Инфо')(Info)
 dag_afisha_bot.message_handler(func=lambda message: message.text in cinemas)(Week)
 
 dag_afisha_bot.message_handler(func=lambda message: message.text in Week.get_week_days())(FilmSchedule)
-
-
-from telebot import apihelper
-
-apihelper.proxy = {'https': 'socks5://207.180.212.121:1080'}
-
-dag_afisha_bot.remove_webhook()
-
-# dag_afisha_bot.polling()
